d70431fe0c11c66bcd3309ab06cae9c9dea2a4ef
paula hage <hage.paula@hotmail.com>
2023-08-14 11:51:00 +0200

    #2 Created the ChecklistRepository to extends the JPA

--- api/src/main/java/com/checklist/api/repository/ChecklistRepository.java
+++ api/src/main/java/com/checklist/api/repository/ChecklistRepository.java
@@ -3,5 +3,5 @@ package com.checklist.api.repository;
 import com.checklist.api.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface ChecklistRepository extends JpaRepository<UserEntity, Long> {
 }
