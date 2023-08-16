491ef210d187cb8067fbecc65d5a40f3aa6862db
paula hage <hage.paula@hotmail.com>
2023-08-16 18:40:37 +0200

    #13 Created the route-guards file

---
+++ ui/src/app/utils/route-guards.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ChecklistService } from '../services/checklist.service';
+
+export const checklistGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+):
+  | Observable<boolean | UrlTree>
+  | Promise<boolean | UrlTree>
+  | boolean
+  | UrlTree => {
+  const user$ = inject(ChecklistService).user;
+  const router = inject(Router);
+
+  return true;
+};
